69a7f58bbde4ae64e959dee6f3026b3ea2994362
Thaynara yasmin silva carlo <thaynara.carlo@alunoescolassociais.org.br>
2024-11-07 12:28:11 -0300

    fiz a conexão de API e finalizei o javascript

--- conversor.js
+++ conversor.js
@@ -1,25 +1,57 @@
-const apiKey = '7b0b4a851916cae1c4aecb84';
-const apiURL = ` https://v6${apiKey}.exchangerate-api.com/v6/7b0b4a851916cae1c4aecb84/latest/`;
-
+const apiKey = 'af7c4b45f7f23b6aa93e18de';
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+ 
 //função para consulta á taxa de câmbio via API
-
-async function getExchangeRate(daMoeda,paraMoeda){
-    try{
-
+ 
+async function getExchangeRate(daMoeda, paraMoeda) {
+    try {
+ 
         const response = await fetch(`${apiURL}${daMoeda}`);
-        const data = response.json();
-
-        if(data.result === "success"){
+        const data = await response.json();
+ 
+        if(data.result === "success") {
             return data.conversion_rates[paraMoeda];
-          
+         
         }else{
-            throw new Error('Erro ao buscar taxa de3 câmbio');
+            throw new Error('Erro ao buscar taxa de câmbio');
         }
-    }catch(error){
+    }catch (error) {
         console.error("Erro:", error);
         return null;
-        
+       
     }
 }
-
-
+ 
+// ###################################################################################################
+// Pega a consulta da API e faz os cálculos
+document.getElementById('currency-form').addEventListener('submit', async function(event){
+    event.preventDefault();
+ 
+    // Obter valores de entrada
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+ 
+    //parseFloat = Ponto Flutuante
+ 
+    const exchangeRate = await getExchangeRate(daMoeda, paraMoeda);
+ 
+    // Ele encontra o valor que deseja converter para o valor convertido e guarda dentro dessa variável
+ 
+ 
+    if(exchangeRate){
+        const convertedValue = valor * exchangeRate; // Guarda o resultado
+ 
+        // console.log(convertedValue);
+ 
+        const conversao = document.getElementById('conversao');
+        conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${paraMoeda}`;
+ 
+        // Configura o botão para executar a conversão
+ 
+    } else{
+        alert('Erro ao buscar a cotação. Tente novamente');
+    }
+ 
+ 
+});
